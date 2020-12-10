667c5b78a032f2d884eee416485a7b04e9c60d97
Ujhazi Balazs <ujhazi.balazs@gmail.com>
2020-12-10 10:19:08 +0100

    Added dto classes

---
+++ src/main/java/hu/unideb/webdev/spring_data/controller/dto/LanguageDto.java
@@ -0,0 +1,14 @@
+package hu.unideb.webdev.spring_data.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LanguageDto {
+    private String name;
+}
